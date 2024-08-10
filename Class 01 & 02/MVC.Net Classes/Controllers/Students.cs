04fe3b752222ba6abaf2024d98a4781c2f21c510
Mubeenahmed143 <ahmedsheikhmubeen395@gmail.com>
2024-08-10 12:43:30 +0500

    (first & second class me view pr kam kra hai)(third class me form me data insert kra hai or fetch kra hai without database)

---
+++ Class 01 & 02/MVC.Net Classes/Controllers/Students.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Net_Classes.Controllers
+{
+    public class Students : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View("aboutus");
+        }
+
+        public IActionResult Url()
+        {
+            return Redirect("https://prium.github.io/elixir/v3.0.0/");
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            int a = 90;
+            int b = 90;
+            return Content("Solve the problem");
+        }
+
+        public IActionResult Details()
+        {
+            int abc = 60;
+            if (abc == null)
+            {
+                return View();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Class 03/Class 03/Controllers/Form.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Class_03.Controllers
+{
+    public class Form : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Index(string fname, string lname)
+        {
+            ViewBag.Name = string.Format("Name {0} {1}" , fname, lname);
+            return View();
+        }
+    }
+}

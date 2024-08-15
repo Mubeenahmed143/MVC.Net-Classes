36c9ab156accf61fd839219b387031176d4f6180
Mubeenahmed143 <ahmedsheikhmubeen395@gmail.com>
2024-08-15 13:18:00 +0500

    viewdata & tempdata

---
+++ Class 04/Class 04/Controllers/DetailViewData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Class_04.Controllers
+{
+    public class DetailViewData : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            ViewBag.ProductTitle = "Toys";  // ViewBag = single value
+            ViewBag.ProductPrice = 500;
+            ViewData["ProductTitle"] = "car toy";  // ViewData = bunch of key value
+            TempData["Productqty"] = "200"; // TempData = ek controller ky dusrt controller ky view mein send kr ta hy 
+            TempData.Keep("Productqty"); // keep = apny pass rakh ta hy
+            return View();
+        }
+    }
+}
---
+++ task (calculator)/task/Controllers/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task.Controllers
+{
+    public class Calculator : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Index(string num1, string num2)
+        {
+            int number1 = Convert.ToInt32(num1);
+            int number2 = Convert.ToInt32(num2);
+
+            int sum = number1 + number2;
+            ViewBag.Name = "Answer :" + sum;
+            return View();
+        }
+    }
+}

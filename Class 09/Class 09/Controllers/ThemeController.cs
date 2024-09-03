8722c2a74dd921d8e61a73aa5ffb4de598bac6fe
Mubeen Ahmed <ahmedsheikhmubeen395@gmail.com>
2024-09-03 13:16:54 +0500

    update

---
+++ Class 09/Class 09/Controllers/ThemeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Class_09.Controllers
+{
+	public class ThemeController : Controller
+	{
+		public IActionResult Index1()
+		{
+			return View();
+		}
+
+        public IActionResult shop()
+        {
+            return View();
+        }
+    }
+}

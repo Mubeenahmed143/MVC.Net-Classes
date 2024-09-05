2c52d30bb3593e506635461866719e46f1026b22
Mubeen Ahmed <ahmedsheikhmubeen395@gmail.com>
2024-09-05 13:50:19 +0500

    Email sending pending work

---
+++ Class 10 (1)/Class 10 (1)/Controllers/WebCont.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Class_10__1_.Controllers
+{
+    public class WebCont : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}

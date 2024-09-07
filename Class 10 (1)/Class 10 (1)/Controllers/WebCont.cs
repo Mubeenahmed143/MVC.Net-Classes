f2545e1f87a8b062097367b76c13013528a28042
Mubeen Ahmed <ahmedsheikhmubeen395@gmail.com>
2024-09-07 13:22:21 +0500

    Email sending work has done and class 11 is cookies work (cookies work is pending class 11)

--- Class 10 (1)/Class 10 (1)/Controllers/WebCont.cs
+++ Class 10 (1)/Class 10 (1)/Controllers/WebCont.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using System.Net.Mail;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace Class_10__1_.Controllers
 {
@@ -9,9 +12,33 @@ namespace Class_10__1_.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		public IActionResult Contact(string email , string mess)
+		{
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+
+            client.Credentials = new NetworkCredential("ahmedsheikhmubeen395@gmail.com", "dzvezzjuhwgoztqh");
+
+            MailMessage mssg = new MailMessage(email, "ahmedsheikhmubeen395@gmail.com");
+            mssg.Subject = "Feedback";
+            mssg.Body = mess + " " + email;
+
+            client.Send(mssg);
+
+            ViewBag.Message = "Mail sent successfully,Role assigned";
+
+
+
+			return View();
+		}
+	}
 }
